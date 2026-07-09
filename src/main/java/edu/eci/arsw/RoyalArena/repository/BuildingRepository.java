42b3d378628a492aa57331c8b7035d1fc8283066
diegoortiz1008-hash <diegoortiz1008@gmail.com>
2026-07-08 21:41:32 -0500

    Correcction of class

--- src/main/java/edu/eci/arsw/RoyalArena/repository/BuildingRepository.java
+++ src/main/java/edu/eci/arsw/RoyalArena/repository/BuildingRepository.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.royalarena.cards.model.Building;
-
 @Repository
 public interface BuildingRepository extends JpaRepository<Building, Long> {
 
