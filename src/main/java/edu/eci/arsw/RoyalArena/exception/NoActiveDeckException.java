9df11fe358b6a6bdff3321506006698e7b957eb7
diegoortiz1008-hash <diegoortiz1008@gmail.com>
2026-07-15 23:44:22 -0500

    move to GameEngine

--- src/main/java/edu/eci/arsw/RoyalArena/exception/NoActiveDeckException.java
+++
@@ -1,8 +0,0 @@
-package edu.eci.arsw.RoyalArena.exception;
-
-/** El jugador no tiene un mazo activo configurado. Es culpa del usuario. */
-public class NoActiveDeckException extends RuntimeException {
-    public NoActiveDeckException(String message) {
-        super(message);
-    }
-}
